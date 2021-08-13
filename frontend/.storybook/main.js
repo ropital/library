de2100c0a377ff2ea0114f95df9fc51d7e158498
Sho Sakai <ropitaru88@gmail.com>
2021-08-13 17:20:55 +0900

    storybookをnextjs17に対応

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,3 +1,15 @@
+const loaders = (options = {}) => [
+  { loader: 'style-loader' },
+  {
+    loader: 'css-loader',
+    options: {
+      importLoaders: 1,
+      ...options,
+    },
+  },
+  { loader: 'postcss-loader' },
+];
+
 module.exports = {
   stories: ['../src/components/**/*.stories.@(tsx|mdx)'],
   addons: [
@@ -7,4 +19,22 @@ module.exports = {
     '@storybook/addon-docs',
     '@storybook/addon-a11y',
   ],
+  webpackFinal: async (config) => ({
+    ...config,
+    module: {
+      ...config.module,
+      rules: config.module.rules.reduce(
+        (acc, cur) => [
+          ...acc,
+          ...cur.test.toString() === /\.css$/.toString()
+            ? [
+              { test: /\.module\.css$/, use: loaders({ modules: true }) },
+              { test: /^.*(?<!\.module)\.css$/, use: loaders() },
+            ]
+            : [cur],
+        ],
+        [],
+      ),
+    },
+  }),
 };
